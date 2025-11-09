d5309faf22e47a167b420104991edd677663fb53
0tito <oplaton@gmail.com>
2025-11-09 12:42:24 -0300

    Add action overlay

---
+++ Content.Client/_RMC14/Actions/ActionOverlay.cs
@@ -0,0 +1,11 @@
+using Robust.Client.Graphics;
+
+namespace Content.Client._RMC14.Actions;
+
+public sealed class ActionOverlay : Overlay
+{
+    protected override void Draw(in OverlayDrawArgs args)
+    {
+    }
+
+}
---
+++ Content.Client/_RMC14/Actions/ActionOverlaySystem.cs
@@ -0,0 +1,11 @@
+using Content.Shared._RMC14.Actions;
+
+namespace Content.Client._RMC14.Actions;
+
+public sealed class ActionOverlaySystem : SharedRMCActionsSystem
+{
+    public override void Initialize()
+    {
+
+    }
+}
